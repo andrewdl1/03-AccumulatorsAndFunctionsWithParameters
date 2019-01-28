bc318a7062e8b54f9d22c1406997fed1c73ef1bb
andrewdl <andrewdl@rose-hulman.edu>
2019-01-28 12:00:27 -0500

    I finished m9.

--- src/m9_using_objects.py
+++ src/m9_using_objects.py
@@ -17,6 +17,8 @@ def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
     two_circles()
+    circle_and_rectangle()
+    lines()
 
 
 def two_circles():
@@ -80,7 +82,7 @@ def circle_and_rectangle():
            150.0
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its green doc-string above.
+    # DONE: 3. Implement this function, per its green doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -88,10 +90,36 @@ def circle_and_rectangle():
     # IMPORTANT: Use the DOT TRICK to guess the names of the relevant
     #       instance variables for outline thickness, etc.
     # -------------------------------------------------------------------------
-    window = rg.RoseWindow()
-    circle = rg.Circle(center_point, 5)
-    circle.fill_color= 'blue'
-    
+    window = rg.RoseWindow(500, 500)
+    center_point = rg.Point(400, 400)
+    center_pointx = 400
+    center_pointy = 400
+    circle = rg.Circle(center_point, 50)
+    circle.attach_to(window)
+    fill = 'blue'
+    circle.fill_color = 'blue'
+    outline = circle.outline_thickness = 3
+    c1 = rg.Point(0, 0)
+    c2 = rg.Point(300, 300)
+    rectangle = rg.Rectangle(c1, c2)
+    centerrect = rg.Point(150, 150)
+    center1 = 150
+    center2 = 150
+    outline1 =rectangle.outline_thickness = 10
+    rectangle.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
+    print(outline)
+    print(fill)
+    print(center_point)
+    print(center_pointx)
+    print(center_pointy)
+    print(outline1)
+    print('none')
+    print(centerrect)
+    print(center1)
+    print(center2)
+
 
 def lines():
     """
@@ -115,7 +143,26 @@ def lines():
 
     -- Waits for the user to press the mouse, then closes the window.
     """
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
+    window = rg.RoseWindow(500, 500)
+    start = rg.Point(10, 10)
+    end = rg.Point(400, 10)
+    midpoint = rg.Point(10, 195)
+    x = 195
+    y = 10
+    line1 = rg.Line(start, end)
+    line1.thickness = 5
+    line1.attach_to(window)
+    started = rg.Point(200, 200)
+    ended = rg.Point(400, 200)
+    line2 = rg.Line(started, ended)
+    line2.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
+    print(midpoint)
+    print(y)
+    print(x)
+
 
 
 # -----------------------------------------------------------------------------
